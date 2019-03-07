8b82cd5822d106113f6c388cace841f06baa897e
Aayla Pozho <aayla@aayla.dev>
2019-03-07 13:35:56 -0800

    That function was kinda useless

--- script.py
+++ script.py
@@ -5,15 +5,12 @@ GIT = None
 def Clone(git, repo): #Run git clone command
     os.system("git clone " + '/'.join(['https:/','github.com',git,repo,]) + ".git %USERPROFILE%\\Documents\\Code\\" + repo)
 
-def ParseJson(json):
-    return json.json()
-
 if (len(sys.argv) == 1): #Test if a profile name is given in the system arguments
     GIT = input("Github username: ")
 else: #Nothing was provided
     GIT = sys.argv[1]
 
-repos = ParseJson(requests.get("http://api.github.com/users/" + GIT + "/repos"))
+repos = requests.get("http://api.github.com/users/" + GIT + "/repos").json()
 os.system("cd /d ")
 for i in repos:
     threading.Thread(target=Clone, args=(GIT, i["name"])).start()
