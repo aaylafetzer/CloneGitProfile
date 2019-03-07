981b3b85c86c34f5787e8dc04e6b0dab9ed67ec8
Aayla Pozho <aayla@aayla.dev>
2019-03-06 18:53:41 -0800

    Initial Commit

---
+++ script.py
@@ -0,0 +1,19 @@
+import os, sys, requests, json
+
+GIT = None
+
+def Clone(git, repo): #Run git clone command
+    os.system("git clone " + '/'.join(['https:/','github.com',git,repo,]) + ".git %USERPROFILE%\\Documents\\Code\\" + repo)
+
+def ParseJson(json):
+    return json.json()
+
+if (len(sys.argv) == 1): #Test if a profile name is given in the system arguments
+    GIT = input("Github username: ")
+else: #Nothing was provided
+    GIT = sys.argv[1]
+
+repos = ParseJson(requests.get("http://api.github.com/users/" + GIT + "/repos"))
+os.system("cd /d ")
+for i in repos:
+    Clone(GIT, i["name"])
