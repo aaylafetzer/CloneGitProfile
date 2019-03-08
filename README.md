31cef7b0b1539b0ef2e5beb712e985720b227962
Aayla Pozho <aayla@aayla.dev>
2019-03-07 17:35:19 -0800

    Made the script platform agnostic and added more comments

--- README.md
+++ README.md
@@ -7,3 +7,10 @@ You can either run the script with no arguments:
 ``python script.py``
 or you can add your github username as an argument to make it go faster:
 ``python script.py anzx42``
+
+## Notes
+This should be obvious, but this script requires git. On Windows this can be downloaded from [git-scm.com](https://git-scm.com/download/win). You can install git on Linux distributions with:
+
+* Debian: ``apt install git``
+* Arch: ``pacman -Sy git``
+* Fedora: ``yum install git-core``
--- script.py
+++ script.py
@@ -1,16 +1,26 @@
-import os, sys, requests, json, threading
+import os, sys, requests, json, threading, platform
 
 GIT = None
-
+PATH = None
+PLATFORM = platform.system()
+    
 def Clone(git, repo): #Run git clone command
-    os.system("git clone " + '/'.join(['https:/','github.com',git,repo,]) + ".git %USERPROFILE%\\Documents\\Code\\" + repo)
+    os.system("git clone " + '/'.join(['https:/','github.com',git,repo]) + ".git " + '/'.join([PATH,repo])) #Use the system command line to run git clone
+
+#Detect what platform the script is running on
+if (PLATFORM == "Linux" or PLATFORM == "Darwin"): #The script is running on Linux or MacOS
+    PATH = "~/Documents/Code/"
+elif (PLATFORM == "Windows"): #The script is running on garbage
+    PATH = "%USERPROFILE%/Documents/Code"
+else: #The script can't identify the host OS
+    input("Unable to identify operating system")
+    exit()
 
 if (len(sys.argv) == 1): #Test if a profile name is given in the system arguments
-    GIT = input("Github username: ")
-else: #Nothing was provided
+    GIT = input("Github username: ") #Nothing was provided
+else: #A name was provided
     GIT = sys.argv[1]
 
-repos = requests.get('/'.join(["http:/","api.github.com","users",GIT,"repos"])).json()
-os.system("cd /d ")
+repos = requests.get('/'.join(["http:/","api.github.com","users",GIT,"repos"])).json() #Query the GitHub API for a list of the user's repositories and store the repsonse as JSON
 for i in repos:
-    threading.Thread(target=Clone, args=(GIT, i["name"])).start()
+    threading.Thread(target=Clone, args=(GIT, i["name"])).start() #Split executions of the Clone function into multiple threads to clone larger profiles faster
